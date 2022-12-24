b5664ec62918d9d9167fd1bf5c88c8ebbd8a02f3
Brian Cho <b.cho@queensu.ca>
2022-12-24 17:35:59 -0500

    Create body

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import Stack from "react-bootstrap/Stack";
-import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Sidebar from "./components/Sidebar";
+import Body from "./components/Body";
 
 function App() {
-  const projects = [
-    {
-      id: 1,
-      title: 'Psychokinefish',
-      description: 'Horizontal scrolling fish game involving psychokinefish.',
-      link: 'https://chobbri.itch.io/psychokinefish',
-    },
-    {
-      id: 2,
-      title: 'Wiggle Match',
-      description: 'Block matching puzzle game',
-      link: 'https://chobbri.itch.io/wiggle-match',
-    },
-  ]
   return (
     <>
     <Container fluid className="App">
       <Header />
-      <Container>
-        <Stack direction="horizontal">
-          <Sidebar />
-          <Container>
-            <Projects />
-          </Container>
-        </Stack>
-      </Container>
+      <Body sidebar>
+        <Projects />
+      </Body>
     </Container>
     </>
   );
